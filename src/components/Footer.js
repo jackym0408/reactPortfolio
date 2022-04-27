d54bdb879bd3c8beee917e13038b453be34bb8a4
Jackeline Mendoza <jackelinemendoza@R2Hs-MBP.lan>
2022-04-27 08:31:19 -0400

    almost

---
+++ src/components/Footer.js
@@ -0,0 +1,25 @@
+import React from 'react'
+
+const footer = () => {
+  return (
+    <>
+    <FooterCont>
+        <FooterWrap>
+            <FooterLinksCont>
+                <FooterLinksWrap>
+                    <FooterLinkItems>
+                        <FooterLinkTitles> </FooterLinkTitles> 
+                            <FooterLink a href="http://www.linkedin.com/"FontAwesomeIcon icon="fa-brands fa-linkedin"/> </>
+                            <FooterLink a href="http://www.github.com/"FontAwesomeIcon icon="fa-brands fa-github" />
+                    </FooterLinkItems>
+                </FooterLinksWrap>
+            </FooterLinksCont>
+        </FooterWrap>
+
+    </FooterCont>
+    
+    </>
+  )
+}
+
+export default footer
